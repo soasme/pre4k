3bd71e3c8aaff5d4261463a5b19c53e62316727a
soasme <soasme@gmail.com>
2016-12-04 09:26:06 +1300

    receive signals and do some action.

--- pre4k.c
+++ pre4k.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <sys/select.h>
 
+void wakeup_master();
+
 int accept_requests = 0;
 int num_workers = 2;
 int graceful_timeout = 20;
@@ -13,7 +15,54 @@ int heartbeat_interval = 5;
 int heartbeat_timeout = 20;
 const char* pidfile = "/tmp/prefork.pid";
 int pipe_fd[2];
-char pipe_buf[1];
+char pipe_buf[2];
+
+typedef struct _queue {
+    int length;
+    int current;
+    int queue[5];
+} Queue ;
+
+Queue queue = {
+    .length = 5,
+    .current = -1,
+};
+
+void
+debug_queue(Queue queue)
+{
+    printf("debug: queue current %d, values: ", queue.current);
+    for (int i = 0; i < queue.length; i++) {
+        printf("%d ", queue.queue[i]);
+    }
+    printf("\n");
+}
+
+int
+push_queue(Queue queue, int elem)
+{
+    queue.current += 1;
+    queue.current %= queue.length;
+    queue.queue[queue.current] = elem;
+    debug_queue(queue);
+    return 0;
+}
+
+int
+pop_queue(Queue queue) {
+    if (queue.current < 0) {
+        printf("error: pre4k can't pop element from empty queue");
+        exit(1);
+    }
+    int elem = queue.queue[queue.current];
+    queue.current -= 1;
+    return elem;
+}
+
+int
+count_queue(Queue queue) {
+    return queue.current;
+}
 
 void
 sigchld_handler(int signo)
@@ -76,9 +125,39 @@ sigwinch_handler(int signo) {
 }
 
 void
+pre4k_buff_signals(int signo) {
+    printf("info: receive signal: %d\n", signo);
+    push_queue(queue, signo);
+    wakeup_master();
+}
+
+void
+register_signals()
+{
+    int signals[9] = {
+        SIGHUP, SIGQUIT, SIGINT, SIGTERM,
+        SIGTTIN, SIGTTOU, SIGUSR1, SIGUSR2,
+        SIGWINCH
+    };
+    for (int i = 0; i < 9; i++) {
+        if (signal(signals[i], pre4k_buff_signals) == SIG_ERR) {
+            printf("register signals for %d failed.\n", signals[i]);
+            exit(1);
+        }
+    }
+}
+
+
+void
 start()
 {
+    // fixme: create pidfile
 
+    register_signals();
+    /*if (signal(SIGTTIN, sigttin_handler) == SIG_ERR) {
+        printf("error: cannot receive sigttin.\n");
+        exit(1);
+    }*/
 }
 
 void
@@ -90,31 +169,35 @@ manage_workers()
 int
 maybe_get_signo()
 {
-    return 0;
+    if (count_queue(queue) == -1) {
+        return 0;
+    } else {
+        int signo = pop_queue(queue);
+        return signo;
+    }
 }
 
 void
 sleep_master()
 {
-    fd_set set;
+    fd_set rset;
     struct timeval timeout;
 
-    FD_ZERO(&set);
-    FD_SET(pipe_fd[0], &set);
+    FD_ZERO(&rset);
+    FD_SET(pipe_fd[0], &rset);
 
     timeout.tv_sec = 1;
     timeout.tv_usec = 0;
 
-    int ready = select(FD_SETSIZE, &set, NULL, NULL, &timeout);
-    if (ready == 0) {
+    int ready = select(FD_SETSIZE, &rset, NULL, NULL, &timeout);
+    if (ready <= 0) {
+        printf("debug: sleeping...\n");
         return;
-    } else if (ready < 0) {
-        printf("error: pre4k can't block pipe\n");
-        exit(1);
     } else {
         // fixme: EAGAIN, EINTR
-        while(read(pipe_fd[0], &pipe_buf, 1)) {
-            printf("go");
+        if (read(pipe_fd[0], &pipe_buf, 1) != 1) {
+            printf("error: unexpected read: %s", pipe_buf);
+            exit(1);
         }
     }
 
@@ -124,7 +207,8 @@ void
 wakeup_master()
 {
     // fixme: EAGAIN, EINTR
-    write(pipe_fd[1], "", 1);
+    write(pipe_fd[1], ".", 1);
+    printf("debug: pre4k wrote . to pipe.\n");
 }
 
 void
